9b8d9e1d41124de088cfa241898394272c43ba95
zhangcongjiang <847634038@qq.com>
2025-04-24 14:52:11 +0800

    speaker增加性别字段

--- voice/models.py
+++ voice/models.py
@@ -22,9 +22,14 @@ class Sound(models.Model):
 
 
 class Speaker(models.Model):
+    GENDER_CHOICES = [
+        ('MALE', '男'),
+        ('FEMALE', '女'),
+    ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField()
     voice_style = models.CharField(max_length=36)  # 对应的pt音色种子文件
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=False)  # 新增性别字段
     spec = models.JSONField(default=dict, null=True, blank=True)
 
 
